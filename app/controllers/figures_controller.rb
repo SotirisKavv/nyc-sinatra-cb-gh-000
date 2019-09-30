47efa18ca61004376aa305462c2256e6b33e51f7
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-30 07:03:07 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -24,7 +24,23 @@ class FiguresController < ApplicationController
 
   get '/figures/:id' do
     @figure = Figure.find_by_id(params[:id])
-
     erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    if !params[:owner].keys.include?("pet_ids")
+    params[:owner]["pet_ids"] = []
+    end 
+    @owner = Owner.find(params[:id])
+    @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect "owners/#{@owner.id}"
   end
 end
